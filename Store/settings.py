adae496762dddbb6e83af5e00ed681bdbf006ddc
Przemek <przemekcalka1@gmail.com>
2026-03-13 12:06:26 +0100

    Add in hello.html list of products

--- Store/settings.py
+++ Store/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'Store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-load_dotenv()
+
 
 DATABASES = {
     'default': {
--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from shop.models import Product
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html', {'name': "Przemek"})
+    try:
+        queryset = Product.objects.filter()
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'hello.html', {'name': "Przemek", 'products':list(queryset)})
