bb8cba86123802f33b7fdbae208929aa9edca6a2
Przemek <przemekcalka1@gmail.com>
2026-03-17 05:07:46 +0100

    Add filters to product page.

--- shop/models.py
+++ shop/models.py
@@ -52,8 +52,10 @@ class Customer(models.Model):
     membership = models.CharField(max_length= 1, choices=MEMBERSHIP_CHOICES, default='B')
 
     def __str__(self):
-        return self.last_name
+        return f'{self.first_name} {self.last_name}'
 
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
